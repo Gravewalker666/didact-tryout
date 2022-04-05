38554aa77573211c1fbc5ae359b9718a9b41e1e5
Gravewalker666 <heshanandrews99@gmail.com>
2022-04-05 23:30:30 +0530

    Appened an element into the dom without react

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
-var newElement = /*#__PURE__*/ React.createElement("h1", null, "This is an h1");
-function newFunction() {
-    console.log("this is complied properly");
-}
+var element = {
+    type: "h1",
+    props: {
+        title: "foo",
+        children: "Hello"
+    }
+};
+var container = document.getElementById("root");
+var node = document.createElement(element.type);
+node["title"] = element.props.title;
+var text = document.createTextNode("");
+text["nodeValue"] = element.props.children;
+node.appendChild(text);
+container.appendChild(node);
 
 
 //# sourceMappingURL=app.js.map
--- app.jsx
+++ app.jsx
@@ -1,5 +1,16 @@
-const newElement = <h1>This is an h1</h1>
-
-function newFunction() {
-    console.log("this is complied properly");
+const element = {
+    type: "h1",
+    props: {
+        title: "foo",
+        children: "Hello",
+    },
 }
+
+const container = document.getElementById("root");
+
+const node = document.createElement(element.type);
+node["title"] = element.props.title;
+const text = document.createTextNode("");
+text["nodeValue"] = element.props.children;
+node.appendChild(text);
+container.appendChild(node);
