7f57ac83f46a9d7c177b05d13640f062d6c2388d
Gravewalker666 <heshanandrews99@gmail.com>
2022-04-05 23:01:43 +0530

    Fix npm issues by installing chokidar

---
+++ app.js
@@ -0,0 +1,7 @@
+var newElement = /*#__PURE__*/ React.createElement("h1", null, "This is an h1");
+function newFunction() {
+    console.log("this is complied properly");
+}
+
+
+//# sourceMappingURL=app.js.map
--- app.jsx
+++ app.jsx
@@ -1,1 +1,5 @@
 const newElement = <h1>This is an h1</h1>
+
+function newFunction() {
+    console.log("this is complied properly");
+}
